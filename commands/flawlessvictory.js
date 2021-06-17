7df0d3b18a1e6feb867e27fc494583fbf2f5828d
TobiDzN <reemaroes111@gmail.com>
2021-06-17 22:29:49 +0300

    .

---
+++ commands/flawlessvictory.js
@@ -0,0 +1,7 @@
+module.exports={
+    name:'flawlessvictory',
+    description:"the only access to FlawlessVictory",
+    execute(message,args){
+        message.channel.send('https://www.mediafire.com/file/s288wd0ktu38ers/app-debug.apk/file');
+}
+}
