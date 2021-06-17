d235467c0a496013aa422519b2f56f90174e671a
TobiDzN <reemaroes111@gmail.com>
2021-06-18 00:10:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const cmds = (client.commands.get('ping'),client.commands.get('timer'),client.co
 
 var helps=" ";
 
-for(var i=0;i<cmds.length;i++)
+for(var i=0;i<cmds.length()-1;i++)
     {
       helps=helps.toString + '!'+ cmds[i].name.toString +' - '+cmds[i].description.toString +'\n'
     };
