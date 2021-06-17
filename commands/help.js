a485a774b521d1d3a98b0851cac92f8a44fa08ff
TobiDzN <reemaroes111@gmail.com>
2021-06-17 23:32:53 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,8 @@ const { Client } = require("discord.js");
 
 const client = new Discord.Client();
 
+const fs = require('fs');
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
