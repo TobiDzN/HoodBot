b75bb3d6f1f75de69e9d7bf00646a39a6ee4889a
TobiDzN <reemaroes111@gmail.com>
2021-06-19 20:53:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -29,6 +29,16 @@ client.once('ready',()=>{
     welcome(client);
 });
 
+client.on("message", message => {
+    if (message.author.bot) return false;
+
+    if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
+
+    if (message.mentions.has(client.user.id)) {
+        message.channel.send("Hello there!");
+    };
+});
+
 client.on('message',message=>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
