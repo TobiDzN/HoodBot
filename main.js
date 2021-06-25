e30e94180c01c733ab52c4a181e988717f169d4b
TobiDzN <reemaroes111@gmail.com>
2021-06-25 21:24:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -34,8 +34,10 @@ client.on("message", message => {
 
     if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
 
-    if (message.mentions.has(356058278918356993)) {
-        message.channel.send("https://cdn.discordapp.com/emojis/611546554838417439.gif?v=1");
+    if(message.content.includes('@Yoshi')) message.channel.send('https://cdn.discordapp.com/emojis/611546554838417439.gif?v=1');
+
+    if (message.mentions.has(client.user.id)) {
+        message.channel.send("Hello there!");
     };
 });
 
