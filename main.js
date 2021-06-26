c00557b9762ec700f1172695e4953ca80e49bba6
TobiDzN <reemaroes111@gmail.com>
2021-06-26 04:47:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -48,9 +48,9 @@ client.on("message", message => {
 client.on('message',message=>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
-    const args = message.content.slice(prefix.length).split(/ +/);
+    let args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
+    
     if(command === 'ping'){
     client.commands.get('ping').execute(message,args);
     }
