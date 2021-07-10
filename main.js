afe071edfede03f649337c1ea8be5ed3a5b15408
TobiDzN <reemaroes111@gmail.com>
2021-07-10 19:16:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -152,7 +152,9 @@ client.on('message',message=>{
     else if(command==='dc')
     {
        // server.queue.push("https://www.youtube.com/watch?v=k6Ly96hHt1A");
-        Thread.sleep(2000);
+        setTimeout(() => {
+            
+        }, 2000);
         
         message.member.voice.channel.leave();
         message.channel.send("Cya dog!");
